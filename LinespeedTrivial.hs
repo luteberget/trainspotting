37c33f02170e3d91b6e96bcbb9c3915ef62909ee
Bjørnar Luteberget <luteberget@gmail.com>
2017-12-11 21:41:41 +0100

    WIP trivial linespeed

--- LinespeedTrivial.hs
+++ LinespeedTrivial.hs
@@ -7,6 +7,8 @@ import Control.Monad (forM_)
 segmentLength = 1000.0
 maxVelocity = 20.0
 maxTime = 1000.0
+maxAccel = 1.1
+maxBrake = 0.75
 
 type Pos = FloatExpr
 type Velocity = FloatExpr
@@ -16,10 +18,31 @@ type DeltaTime = FloatExpr
 type State = (Pos, Velocity)
 type Transition = (DeltaPos, DeltaTime)
 
-transition :: FloatSolver -> State -> State -> IO Transition
-transition fs s1 s2 = do
+transition :: Solver -> FloatSolver -> State -> State -> IO Transition
+transition s fs s1 s2 = do
   dx <- newFloat fs 0.0 segmentLength
   dt <- newFloat fs 0.0 maxTime
+  let (v1,v2) = (snd s1, snd s2)
+
+  brakingDecision <- newLit s 
+  isBraking <- mkFloatConstraint fs (v2 .<=. v1)
+
+  let brakeDiff = (square (v1 .-. v2)) .*. (floatConst (1.0/(2.0*maxBrake)))
+  let accelDiff = (square (v1 .-. v2)) .*. (floatConst (1.0/(2.0*maxAccel)))
+
+  addClause s [neg brakingDecision, isBraking]
+  addClause s [brakingDecision, neg isBraking]
+  
+  braking1  <- mkFloatConstraint fs (((dt .*. v2) .+. brakeDiff) .<=. dx)
+  braking2  <- mkFloatConstraint fs (dx .<=. ((dt .*. v1) .-. brakeDiff))
+  addClause s [neg brakingDecision, braking1]
+  addClause s [neg brakingDecision, braking2]
+
+  acceling1 <- mkFloatConstraint fs (((dt .*. v1) .+. accelDiff) .<=. dx)
+  acceling2 <- mkFloatConstraint fs (dx .<=. ((dt .*. v2) .-. accelDiff))
+  addClause s [brakingDecision, acceling1]
+  addClause s [brakingDecision, acceling2]
+
   return (dx,dt)
 
 main = withNewSolver $ \s -> do
@@ -30,7 +53,7 @@ main = withNewSolver $ \s -> do
   midStateVelocity <- newFloat fs 0.0 maxVelocity
   let endState   = (floatConst segmentLength, floatConst 0.0)
   let states = [startState, (midStatePos, midStateVelocity), endState]
-  transitions <- sequence [ transition fs s1 s2 
+  transitions <- sequence [ transition s fs s1 s2 
                           | (s1,s2) <- states `zip` tail states ]
 
   b <- solveWithFloat fs
@@ -45,13 +68,13 @@ main = withNewSolver $ \s -> do
 
 stateToString :: FloatSolver -> State -> IO String
 stateToString fs (pos,vel) = do
-  posVal <- floatValue pos
-  velVal <- floatValue vel
+  posVal <- evalFloatExpr fs pos
+  velVal <- evalFloatExpr fs vel
   return $ " - State: x=" ++ (show posVal) ++ ", v=" ++ (show velVal)
 
 transitionToString :: FloatSolver -> Transition -> IO String
 transitionToString fs (dx, dt) = do 
-  dxVal <- floatValue dx
-  dtVal <- floatValue dt
+  dxVal <- evalFloatExpr fs dx
+  dtVal <- evalFloatExpr fs dt
   return $ " - Transition: dx=" ++ (show dxVal) ++ ", dt=" ++ (show dtVal)
 
