00ce50e93fa308e1806352cb6dcfdb7e532f6177
Bjørnar Luteberget <luteberget@gmail.com>
2017-12-15 15:23:55 +0100

    Testing linespeed trivial with cleaned up solver

--- LinespeedTrivial.hs
+++ LinespeedTrivial.hs
@@ -1,14 +1,15 @@
 module Main where
 
 import SAT
+import SAT.Bool
 import SAT.FloatTheory
 import Control.Monad (forM_)
 
-segmentLength = 1000.0
-maxVelocity = 10.00
+segmentLength = 100.0
+maxVelocity = 1.00
 maxTime = 5000.0
-maxAccel = 2.0
-maxBrake = 1.0
+maxAccel = 0.02
+maxBrake = 90.0
 
 type Pos = FloatExpr
 type Velocity = FloatExpr
@@ -24,46 +25,43 @@ transition maxT s fs s1 s2 = do
   dt <- newFloat fs 0.0 maxT
   let (v1,v2) = (snd s1, snd s2)
 
-  brakingDecision <- newLit s 
-  isBraking <- mkFloatConstraint fs (v2 .<=. v1)
 
-  let brakeDiff = (square (v1 .-. v2)) .*. (floatConst (1.0/(2.0*maxBrake)))
-  let accelDiff = (square (v1 .-. v2)) .*. (floatConst (1.0/(2.0*maxAccel)))
+  let brakeDiff = (square (v1 .-. v2)) .*. (number (1.0/(2.0*maxBrake)))
+  let accelDiff = (square (v1 .-. v2)) .*. (number (1.0/(2.0*maxAccel)))
 
-  addClause s [neg brakingDecision, isBraking]
-  addClause s [brakingDecision, neg isBraking]
-  
-  braking1  <- mkFloatConstraint fs (((dt .*. v2) .+. brakeDiff) .<=. dx)
-  braking2  <- mkFloatConstraint fs (dx .<=. ((dt .*. v1) .-. brakeDiff))
-  addClause s [neg brakingDecision, braking1]
-  addClause s [neg brakingDecision, braking2]
+  isBraking  <- constraint fs (v2 .<=. v1)
+  braking1  <- constraint fs (((dt .*. v2) .+. brakeDiff) .<=. dx)
+  braking2  <- constraint fs (dx .<=. ((dt .*. v1) .-. brakeDiff))
 
-  acceling1 <- mkFloatConstraint fs (((dt .*. v1) .+. accelDiff) .<=. dx)
-  acceling2 <- mkFloatConstraint fs (dx .<=. ((dt .*. v2) .-. accelDiff))
-  addClause s [brakingDecision, acceling1]
-  addClause s [brakingDecision, acceling2]
+  isAcceling <- constraint fs (v2 .>=. v1)
+  acceling1 <- constraint fs (((dt .*. v1) .+. accelDiff) .<=. dx)
+  acceling2 <- constraint fs (dx .<=. ((dt .*. v2) .-. accelDiff))
 
-  travelDistance <- mkFloatConstraint fs ((fst s1) .+. dx .==. (fst s2))
-  addClause s [travelDistance]
+  acc <- andl s [isAcceling, acceling1, acceling2]
+  brk <- andl s [isBraking, braking1, braking2]
+  addClause s [acc, brk]
+  addClause s [neg acc, neg brk]
+
+  assertConstraint fs ((fst s1) .+. dx .==. (fst s2))
 
   return (dx,dt)
 
 run maxT = withNewSolver $ \s -> do
   fs <- newFloatSolver s
   
-  let startState = (floatConst 0.0, floatConst 0.0)
+  let startState = (number 0.0, number 0.0)
   midStatePos <- newFloat fs 0.0 segmentLength -- 502.806 502.807--0.0 segmentLength
   midStateVelocity <- newFloat fs 0.0 maxVelocity
-  let endState   = (floatConst segmentLength, floatConst 0.0)
+  let endState   = (number segmentLength, number 0.0)
   let states = [startState, (midStatePos, midStateVelocity), endState]
   transitions <- sequence [ transition maxT s fs s1 s2 
                           | (s1,s2) <- states `zip` tail states ]
 
   let sumT = foldl1 (.+.) (map snd transitions)
-  constraint <- mkFloatConstraint fs (sumT .<=. (floatConst maxT))
+  constraint <- constraint fs (sumT .<=. (number maxT))
   addClause s [constraint]
 
-  b <- solveWithFloat fs sumT
+  b <- solveMinimizeFloat fs sumT
   if b then do
     putStrLn "*** Solution:"
     forM_ (states `zip` transitions) $ \(st,tr) -> do
@@ -75,14 +73,14 @@ run maxT = withNewSolver $ \s -> do
 
 stateToString :: FloatSolver -> State -> IO String
 stateToString fs (pos,vel) = do
-  posVal <- evalFloatExpr fs pos
-  velVal <- evalFloatExpr fs vel
+  posVal <- SAT.FloatTheory.modelValue fs pos
+  velVal <- SAT.FloatTheory.modelValue fs vel
   return $ " - State: x=" ++ (show posVal) ++ ", v=" ++ (show velVal)
 
 transitionToString :: FloatSolver -> Transition -> IO String
 transitionToString fs (dx, dt) = do 
-  dxVal <- evalFloatExpr fs dx
-  dtVal <- evalFloatExpr fs dt
+  dxVal <- SAT.FloatTheory.modelValue fs dx
+  dtVal <- SAT.FloatTheory.modelValue fs dt
   return $ " - Transition: dx=" ++ (show dxVal) ++ ", dt=" ++ (show dtVal)
 
 main = run maxTime
