a46c4177d810493d87c979b3cc814dbaa3a83337
Bjørnar Luteberget <luteberget@gmail.com>
2018-03-19 19:15:16 +0100

    unused

--- src/objects/signal.rs
+++
@@ -1,9 +0,0 @@
-use observable::*;
-use railway::{TrainVisitable};
-
-pub struct Signal {
-    authority: Observable<Option<f64>>,
-}
-
-// Nothing happens when train arrives or leaves at signal
-impl TrainVisitable for Signal {}
