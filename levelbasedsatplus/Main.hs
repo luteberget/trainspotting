fb165e3fdeb70cccef4efbca7c59401cf9bd7a47
Bjørnar Luteberget <luteberget@gmail.com>
2018-11-30 10:30:25 +0100

    levelbasedsatplus: lt-relation can be derived from data

--- levelbasedsatplus/Main.hs
+++ levelbasedsatplus/Main.hs
@@ -7,6 +7,25 @@ import SAT.Order
 import SAT.Equal
 import SAT.Optimize
 import Control.Monad (forM, forM_)
+import Control.Applicative ((<|>))
+
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Data.Set (Set)
+import Data.Map (Map)
+import Data.Maybe
+import Data.List (partition)
+
+
+-- # Idea for optimizing with Unary
+-- Summing a large number of Unarys takes up a lot of space.
+-- We can instead do lexicographic optimizatino on each of them,
+--   then sum them into a Unary with upper bound given by the 
+--   result of the lexicographic optimization.
+-- 1. Put in constraints
+-- 2. Optimize each unary in turn, giving upper bound.
+-- 3. Remove the constraints from (2)
+-- 4. Sum the unarys up-to upper bound, optimize this number.
 
 --
 -- INPUT REPRESENTATION
@@ -16,7 +35,7 @@ data Side = SLeft | SRight
 data Dir = Up | Down
   deriving (Show)
 data Port = PLeft | PRight | PTrunk | PBegin | PEnd
-  deriving (Show)
+  deriving (Show, Eq)
 
 type EdgeRef = Int
 type NodeRef = Int
@@ -48,15 +67,23 @@ example2 = SolverInput n e lt
               ((1, PRight),(3,PEnd))]
          lt = [(2,1)]
 
+example2b = SolverInput n e lt
+  where  n = [BeginNode, SwitchNode SLeft Up, SwitchNode SRight Down, EndNode ]
+         e = [((0, PBegin),(1,PTrunk)),
+              ((1, PLeft),(2,PRight)),
+              ((1, PRight),(2,PLeft)),
+              ((2, PTrunk),(3,PEnd)) ]
+         lt = [(2,1)]
+
 example3 = SolverInput n e lt
   where  n = [BeginNode, BeginNode, SwitchNode SLeft Up, 
               SwitchNode SLeft Down,
               EndNode, EndNode ]
-         e = [((0,PBegin),(2,PTrunk)), -- 0  1--4
-              ((1,PBegin),(3,PRight)), -- 1   2
-              ((2,PLeft),(3,PLeft)),   -- 2  0--3
-              ((2,PRight),(4,PEnd)),   -- 3
-              ((3,PTrunk),(5,PEnd))]   -- 4
+         e = [((0,PBegin),(2,PTrunk)), -- 0  edges:  1--4
+              ((1,PBegin),(3,PRight)), -- 1           2
+              ((2,PLeft),(3,PLeft)),   -- 2          0--3
+              ((2,PRight),(4,PEnd)),   -- 3  nodes:   1   3 5
+              ((3,PTrunk),(5,PEnd))]   -- 4           0 2   4
          lt = [(0, 2), (2, 1), (0, 1), (3,2),(3,4),(2,4)]
 
 example4 = SolverInput n e lt
@@ -69,6 +96,14 @@ example4 = SolverInput n e lt
               ((4,PTrunk),(5,PEnd)), -- 4
               ((1,PLeft),(4,PRight)), -- 5
               ((2,PLeft),(3,PRight))] -- 6
+
+---  
+--
+--        /---5----\
+--       /  /-6-\   \
+--    -0x-1x--2--x-3-x-4-
+-- 15 26 65 35 (25)
+--
              
          lt = [(1,5),(2,5),(3,5),(2,6),(5,6)]
 
@@ -90,20 +125,140 @@ example5 = SolverInput n e lt
 --    1      5
 -- -0---2-----\ -6
 --
+-- from 1-2  --> 1 < 2, 1 < 4, 
 --
 --
          lt = [(2,1),(2,4),(2,3),(2,5),(4,3)]
 
 main = withNewSolver $ \s -> do
   let p = example5
-  layout s (sNodes p) (sEdges p) (sEdgeLT p) 10
+  layout s (sNodes p) (sEdges p) 10
  
+
+
+edgePath :: [Node] -> [Edge] -> (EdgeRef,EdgeRef) -> Maybe [(EdgeRef,EdgeSide,Dir)]
+edgePath nodes edges (start,goal) = go [[(start,Begin,Down)],[(start,End,Up)]] (Set.fromList [(start,Begin),(start,End)])
+  where
+    next :: (EdgeRef, EdgeSide, Dir) -> [(EdgeRef,EdgeSide, Dir)]
+    next (e,side,dir) = case (side,dir) of
+                          (Begin,Down) -> nodeLinks
+                          (End, Up) -> nodeLinks
+                          _ -> (e, opposite side, dir)
+      where
+        edgeNodeIdx = fst ((if side == Begin then fst else snd) (edges !! e))
+        nodeLinks = [ (ei,sd) | (ei,es) <- zip [0..] (fmap edgeLink edges), (sd,ni) <- es, ni == edgeNodeIdx ]
+
+    edgeLink ((n1,p1),(n2,p2)) = [(Begin,n1), (End,n2)]
+
+    opposite Begin = End
+    opposite End = Begin
+
+    other Up = Down
+    other Down = Up
+
+    go :: [[(EdgeRef,EdgeSide)]] -> Set (EdgeRef,EdgeSide) -> Maybe [(EdgeRef,EdgeSide)]
+    go [] _ = Nothing 
+    go paths visited = (listToMaybe [ p | p <- newPaths, fst (head p) == goal ]) <|> goNext
+      where newPaths = [ (new:path) | path <- paths, new <- next (head path), not (new `Set.member` visited) ]
+            goNext = go newPaths (Set.union visited $ Set.fromList [ head p | p <- newPaths ])
+
+-- transitive reduction, remove any (a,z) s.t. exits b,c,d,...y and (a,b),(b,c),(c,d),...,(y,z)
+-- Assumes acyclic relation! Reflexivity is ok.
+minRel :: [(Int,Int)] -> [(Int,Int)]
+minRel xs = [ (a,c) | (a,c) <- xs, not (reachable c [ b | (_,b) <- next a, b /= c ]) ]
+  where 
+    s = Set.fromList xs
+    next x = Set.toList (setRangeInclusive s x x)
+    reachable :: Int -> [Int] -> Bool
+    reachable goal [] = False
+    reachable goal (x:xs) | x == goal = True
+    reachable goal (x:xs) = reachable goal ([ b | (_,b) <- next x ] ++ xs)
+
+setRangeInclusive :: Set (Int,Int) -> Int -> Int -> Set (Int,Int)
+setRangeInclusive s lo hi = justRight
+  where (_, highEnough) = Set.split ((lo-1),2^29-1) s
+        (justRight, _ ) = Set.split ((hi+1),-2^29) highEnough
+
+
+type Prio = Set (Int, EdgeRef)
+--
+-- 2018-11-29: ported less-than relation function from Rust. Hm, that wasn't too bad.
+--
+ltRel :: [Node] -> [Edge] -> [(EdgeRef, EdgeRef)]
+ltRel nodes edges = Set.toList $ Set.fromList allLt
+  where
+    (switchLeftEdge,switchRightEdge,switchTrunkEdge) = ((Map.!) switchLeftEdges, (Map.!) switchRightEdges, 
+                                                        (Map.!) switchTrunkEdges)
+    switchLeftEdges  = Map.fromList [ (ni, ei) | (ei,e) <- zip [0..] edges, (ni, PLeft)  <- [fst e, snd e] ]
+    switchRightEdges = Map.fromList [ (ni, ei) | (ei,e) <- zip [0..] edges, (ni, PRight) <- [fst e, snd e] ]
+    switchTrunkEdges = Map.fromList [ (ni, ei) | (ei,e) <- zip [0..] edges, (ni, PTrunk) <- [fst e, snd e] ]
+
+    dirFactor :: Dir -> Int
+    dirFactor Up = 1
+    dirFactor Down = -1
+
+    allLt = [ (under,over) | (i, (SwitchNode side dir)) <- zip [0..] nodes
+                           , let (overs,unders) = findIt i dir
+                           , over <- Set.toList overs, under <- Set.toList unders ]
+
+    findIt :: Int -> Dir -> (Set EdgeRef, Set EdgeRef)
+    findIt n dir = go (Set.singleton (edgeNode (top n)))
+                      (Set.singleton (edgeNode (bottom n)))
+                      (Set.singleton (f*(edgeNode (top n)), top n)) 
+                      (Set.singleton (f*(edgeNode (bottom n)), bottom n))
+                      Set.empty Set.empty
+      where
+        go :: Set NodeRef -> Set NodeRef -> Prio -> Prio -> Set EdgeRef -> Set EdgeRef -> (Set EdgeRef, Set EdgeRef)
+        go oNodes uNodes oQueue uQueue oEdges uEdges | Set.null oQueue || Set.null uQueue || not (Set.null (Set.intersection oNodes uNodes))  = (rest oEdges oQueue, rest uEdges uQueue)
+        go oNodes uNodes oQueue uQueue oEdges uEdges = if overPos < underPos then goOver else goUnder
+          where 
+            ((overPos,overEdge),(underPos,underEdge)) = (Set.elemAt 0 oQueue, Set.elemAt 0 uQueue)
+            goOver  = go newONodes uNodes newOQueue uQueue (Set.insert overEdge oEdges) uEdges
+            goUnder = go oNodes newUNodes oQueue newUQueue oEdges (Set.insert underEdge uEdges)
+            newONodes = Set.union oNodes (Set.fromList [ edgeNode e | e <- nextEdges (edgeNode overEdge)  ])
+            newUNodes = Set.union uNodes (Set.fromList [ edgeNode e | e <- nextEdges (edgeNode underEdge) ])
+            newOQueue = Set.union (Set.deleteAt 0 oQueue) 
+                          (Set.fromList [ (f*(edgeNode e), e) | e <- nextEdges (edgeNode overEdge)])
+            newUQueue = Set.union (Set.deleteAt 0 uQueue)
+                          (Set.fromList [ (f*(edgeNode e), e) | e <- nextEdges (edgeNode underEdge)])
+
+        f = dirFactor dir
+
+        top ni = case (dir, (nodes !! ni)) of 
+                  (Up, SwitchNode _ Up) -> switchLeftEdge ni
+                  (Down, SwitchNode _ Down) -> switchRightEdge ni
+                  _ -> error "top ni"
+
+        bottom ni = case (dir, (nodes !! ni)) of
+                     (Up, SwitchNode _ Up) -> switchRightEdge ni
+                     (Down, SwitchNode _ Down) -> switchLeftEdge ni
+                     _ -> error "bottom ni"
+
+        edgeNode ei = case dir of
+                        Up   -> fst (snd (edges!!ei))
+                        Down -> fst (fst (edges!!ei))
+
+        nextEdges ni = case (dir, (nodes !! ni)) of
+                              (Up, EndNode) -> []
+                              (Up, SwitchNode _ Up) -> [switchLeftEdge ni, switchRightEdge ni]
+                              (Up, SwitchNode _ Down) -> [switchTrunkEdge ni]
+ 
+                              (Down, BeginNode) -> []
+                              (Down, SwitchNode _ Down) -> [switchLeftEdge ni, switchRightEdge ni]
+                              (Down, SwitchNode _ Up) -> [switchTrunkEdge ni]
+
+                              _ -> error "nextEdges ni"
+
+    rest :: Set EdgeRef -> Prio -> Set EdgeRef
+    rest edges queue = Set.union edges (Set.map snd queue)
+
 
 --
 -- SOLVER REPRESENTATION
 --
 
 data EdgeSide = Begin | End
+  deriving (Show, Ord, Eq)
 data PortShape = PortShape {
   goUp :: Lit,  -- going upwards when moving left to right
   goStraight :: Lit,
