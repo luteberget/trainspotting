afd2a573a0e0e33595ce646fe1e9637443f0e784
Bjørnar Luteberget <luteberget@gmail.com>
2019-03-19 20:30:20 +0100

    planner: wip

--- planner/src/problem.rs
+++ planner/src/problem.rs
@@ -1,51 +1,50 @@
 use rolling::input::staticinfrastructure::{NodeId, RouteEntryExit};
 use crate::movement::*;
+use std::collections::HashMap;
 
 
 // Plan interface
 
 pub struct Config {
-    n_before: usize,
-    n_after: usize,
+    pub n_before: usize,
+    pub n_after: usize,
 }
 
-type RoutePartId = (usize,usize); // Route index and partial index into route
-type OverlapId = usize;
-type TrainId = usize;
+pub type PartialRouteId = (usize,usize); // Route index and partial index into route
+pub type OverlapId = usize;
+pub type TrainId = usize;
 
-pub struct RoutePart {
-    name :RoutePartId,
-    entry: RouteEntryExit,
-    exit: RouteEntryExit,
-    conflicts: Vec<Vec<(RoutePartId, usize)>>, // ??
-    wait_conflict :Option<OverlapId>,
-    contains_nodes :Vec<NodeId>,
-    length: f32,
+pub struct PartialRoute {
+    pub entry: RouteEntryExit,
+    pub exit: RouteEntryExit,
+    pub conflicts: Vec<Vec<(PartialRouteId, usize)>>, // ??
+    pub wait_conflict :Option<OverlapId>,
+    pub contains_nodes :Vec<NodeId>,
+    pub length: f32,
 }
 
-type ElementaryRoute = Vec<RoutePartId>;
+type ElementaryRoute = Vec<PartialRouteId>;
 
 pub struct Train {
-    name :String,
-    length: f32,
-    visits: Vec<Vec<NodeId>>,
-    vehicle: Vehicle,
+    pub length: f32,
+    pub visits: Vec<Vec<NodeId>>,
+    pub vehicle: Vehicle,
 }
 
 pub struct TrainOrd {
-    a :(TrainId, VisitId),
-    b :(TrainId, VisitId),
+    pub a :(TrainId, VisitId),
+    pub b :(TrainId, VisitId),
 }
 
 pub struct Problem {
-    routes: Vec<RoutePart>,
-    elementary_routes: Vec<ElementaryRoute>,
-    trains: Vec<Train>,
-    train_ord: Vec<TrainOrd>,
+    pub partial_routes: HashMap<PartialRouteId, PartialRoute>,
+    pub elementary_routes: Vec<ElementaryRoute>,
+    pub trains: HashMap<TrainId,Train>,
+    pub train_ord: Vec<TrainOrd>,
 }
 
 
 // OUTPUT: route plan
 
-pub type RoutePlan = Vec<Vec<(RoutePartId, Option<TrainId>)>>;
+pub type RoutePlan = Vec<Vec<(PartialRouteId, Option<TrainId>)>>;
 
--- planner/src/solver.rs
+++ planner/src/solver.rs
@@ -8,13 +8,10 @@ use crate::problem::*;
 
 // State management types (private)
 
-type RoutePartId = usize;
-type TrainId = usize;
-type OverlapId = usize;
 
 struct State {
-    occupation: HashMap<RoutePartId, Occupation>,
-    trains:     HashMap<TrainId,     TrainState>,
+    partial_routes: HashMap<PartialRouteId, Occupation>,
+    trains: HashMap<TrainId, TrainState>,
 }
 
 struct Occupation {
@@ -23,7 +20,7 @@ struct Occupation {
 }
 
 struct TrainState {
-    progress_before: Vec<(RoutePartId, Lit)>,
+    progress_before: Vec<(PartialRouteId, Lit)>,
     born_before:     Lit,
     visit_before:    Vec<(Vec<NodeId>, Lit)>,
 }
@@ -34,68 +31,124 @@ pub fn plan<F : Fn(&RoutePlan) -> bool>(config :&Config, problem :&Problem, test
     let mut s = Solver::new();
     let mut failed_steps = None;
 
-    debug!("Adding state {}", state.len());
+    debug!("Adding initial state");
     let mut states = Vec::new();
     states.push(mk_state(&mut s, problem));
 
-    let test_solution = |model| {
-        // UNSAT = add new state or abort
-        if let Err(()) = model { 
+    loop {
+        debug!("Solving with n={}.", states.len());
+        if let Ok(model) = s.solve_under_assumptions(
+            end_state_condition(states.last().unwrap())) {
+
+            let schedule = mk_schedule(&model);
+
+            // Built-in checks:
+            // Loop check
+            if let Err(loop_) = loop_check(&schedule) {
+                debug!("Loop check failed. Removing loop.");
+                disallow_loop(&mut s, &loop_);
+                continue;
+            } 
+            debug!("Loop check succeeded.");
+            // Repeat check
+            if let Err(repeat) = repeat_check(&problem, &schedule) {
+                debug!("Repeat check failed. Removing.");
+                disallow_repeat(&mut s, &repeat);
+                continue;
+            }
+            debug!("Repeat check succeeded.");
+
+            // User check: e.g. simulation
+            if !test(&schedule) {
+                debug!("User check failed.");
+                disallow_schedule(&mut s, &schedule);
+                continue;
+            }
+            debug!("User check suceeded, returning resulting plan.");
+            failed_steps = Some(0);
+
+            // Everything is ok
+            break Some(schedule);
+
+        } else {
+            debug!("No more plans for n={}", states.len());
             let increase = match failed_steps {
-                None => state.len() < config.n_before,
+                None => states.len() < config.n_before,
                 Some(f) => f < config.n_after,
             };
-
-            return if increase { Action::AddState } else { Action::Fail };
-        };
-
-        // Make schedule
-        let schedule = get_schedule(problem, model);
-
-        // LOOP CHECK
-        if let Err(loop_) = loop_check(schedule) {
-            return Action::RemoveLoop(loop_);
-        }
-
-        // REPEAT CHECK
-        if let Err(repeat) = repeat_check(schedule) {
-            return Action::RemoveRepeat(repeat);
-        }
-
-        // TEST FUNCTION
-        if test(schedule) {
-             Action::Finished(schedule)
-        } else {
-            Action::Next
-        }
-    };
-
-    loop {
-        debug!("Solving for n_states={}", state.len());
-        enum Action { AddState, Fail }
-
-
-        match test_solution() {
-            Action::Fail => {
-                debug!("Solver failed.");
-                break;
-            },
-            Action::AddState => {
-                debug!("Add new state (start at the top of the loop again.");
+            failed_steps = failed_steps.map(|x| x+1);
+            if increase {
+                debug!("Adding new state.");
                 states.push(mk_state(&mut s, problem));
-            },
-            Action::RemoveSchedule(schedule) => {
-                delete(schedule);
-            },
-            Action::RemoveLoop(loop_) => {
-            },
+                continue;
+            } else {
+                break None;
+            };
         }
     }
 }
 
+
 fn mk_state(s :&mut Solver, problem :&Problem) -> State {
+    use std::iter::{once};
+
+    // Each partial route can be occupied by a train,
+    // and if so, there is (might be) a choice of overlap.
+    let partial_routes :HashMap<PartialRouteId, Occupation> = problem.partial_routes.iter()
+        .map(|(name,r)| (*name,Occupation {
+            occupation: Symbolic::new(s, 
+              once(None).chain((0..problem.trains.len()).map(|x| Some(x))).collect()),
+            overlap_choice: Symbolic::new(s, (0..r.conflicts.len()-1).collect())
+        })).collect();
+
+    // Conflicting routes are excluded
+    for (r1n,r1) in partial_routes.iter() {
+        for overlap in r1.overlap_choice.domain() {
+            for (confl_r, confl_overlap) in &problem.partial_routes[r1n].conflicts[*overlap] {
+                let r2 = &partial_routes[confl_r];
+                s.add_clause(vec![
+                    r1.occupation.has_value(&None),
+                    !r1.overlap_choice.has_value(&overlap),
+                    r2.occupation.has_value(&None),
+                    !r2.overlap_choice.has_value(&confl_overlap)]);
+                    
+            }
+        }
+    }
+
+
+    let trains = unimplemented!();
+
+    State { partial_routes, trains }
 }
 
+fn mk_schedule(model :&Model) -> RoutePlan {
+    unimplemented!()
+}
+
+fn end_state_condition(state :&State) -> Vec<Bool> {
+    unimplemented!()
+}
+
+pub struct Loop {}
+fn loop_check(plan :&RoutePlan) -> Result<(), Loop> {
+    unimplemented!()
+}
+
+pub struct Repeat {}
+fn repeat_check(problem :&Problem, plan :&RoutePlan) -> Result<(), Repeat> {
+    unimplemented!()
+}
+
+
+fn disallow_loop(s :&mut Solver, loop_ :&Loop) {
+}
+
+fn disallow_repeat(s :&mut Solver, repeat :&Repeat) {
+}
+
+fn disallow_schedule(s :&mut Solver, plan :&RoutePlan) {
+}
 
 
 
